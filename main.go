da746e1e2ab666fb804f39012ebca5a0a7e6305f
SimonStnn <simon.stijnen.23@gmail.com>
2024-01-08 21:03:04 +0100

    Add my first function

--- main.go
+++ main.go
@@ -2,7 +2,15 @@ package main
 
 import "fmt"
 
+// First function
+func add(x int, y int) int {
+	return x + y
+}
+
 func main() {
 	// Print "Hello, World!" to the terminal.
 	fmt.Println("Hello, World!")
+
+	// Print the result of the add function.
+	fmt.Println(add(42, 13))
 }
